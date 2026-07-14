cefadcc8ec2f83ec9e2dbcb6e6c51b2de562dc3c
prakhar2301 <119030000+prakhar2301@users.noreply.github.com>
2026-07-14 22:34:06 +0530

    Update github.js

--- api/github.js
+++ api/github.js
@@ -2,14 +2,18 @@ export default async function handler(req, res) {
 
     console.log("========== NEW REQUEST ==========");
 
-    console.log("Method:", req.method);
+    console.log("Method:");
+    console.log(req.method);
 
-    console.log("Headers:", req.headers);
+    console.log("Headers:");
+    console.log(req.headers);
 
-    console.log("Body:", req.body);
+    console.log("Body:");
+    console.log(JSON.stringify(req.body, null, 2));
 
     return res.status(200).json({
         success: true,
         message: "Webhook received successfully"
     });
+
 }
